57c26c1d78d2d66c233bcd1553322e03d0a10729
koachan-kA2 <koadayo.h4@gmail.com>
2026-01-31 16:20:52 +0900

    home.js を更新

--- home.js
+++ home.js
@@ -18,4 +18,34 @@ onAuthStateChanged(auth, user => {
 document.getElementById("logoutBtn").onclick = async () => {
   await signOut(auth);
   location.href = "login.html";
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5KQPd73BJS7k6ueZP8KjyEz78LQfhzek",
+  authDomain: "offline-meet-up.firebaseapp.com",
+  projectId: "offline-meet-up",
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+  signOut
+};
+</script>
+};
+
